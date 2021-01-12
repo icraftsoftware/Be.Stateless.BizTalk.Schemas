2219b595059758623bfbdbd5693711085ef759ae
François Chabot <fchabot@skynet.be>
2021-01-12 09:48:51 +0100

    fix property clashes with BizTalk.Abstractions

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 ﻿# Be.Stateless.BizTalk.Schemas
 
-# [![Build Status](https://dev.azure.com/icraftsoftware/be.stateless/_apis/build/status/Be.Stateless.BizTalk.Schemas%20Manual%20Release?branchName=master)](https://dev.azure.com/icraftsoftware/be.stateless/_build/latest?definitionId=666&branchName=master)
-[![GitHub Release](https://img.shields.io/github/v/release/icraftsoftware/Be.Stateless.BizTalk.Schemas)](https://github.com/icraftsoftware/Be.Stateless.BizTalk.Schemas/releases/latest)
-[![NuGet Version](https://img.shields.io/nuget/v/Be.Stateless.BizTalk.Schemas.svg?style=flat)](https://www.nuget.org/packages/Be.Stateless.BizTalk.Schemas/)
+[![Build Status](https://dev.azure.com/icraftsoftware/be.stateless/_apis/build/status/Be.Stateless.BizTalk.Schemas%20Manual%20Release?branchName=master)](https://dev.azure.com/icraftsoftware/be.stateless/_build/latest?definitionId=72&branchName=master)
+[![GitHub Release](https://img.shields.io/github/v/release/icraftsoftware/Be.Stateless.BizTalk.Schemas?label=Release)](https://github.com/icraftsoftware/Be.Stateless.BizTalk.Schemas/releases/latest)
 
 BizTalk Factory's document and property schemas for general purpose BizTalk Server development.
+
+## NuGet Packages
+
+[![NuGet Version](https://img.shields.io/nuget/v/Be.Stateless.BizTalk.Schemas.svg?label=Be.Stateless.BizTalk.Schemas&style=flat)](https://www.nuget.org/packages/Be.Stateless.BizTalk.Schemas/)
+
+[![NuGet Version](https://img.shields.io/nuget/v/Be.Stateless.BizTalk.Schema.Unit.svg?label=Be.Stateless.BizTalk.Schema.Unit&style=flat)](https://www.nuget.org/packages/Be.Stateless.BizTalk.Schema.Unit/)
---
+++ src/Be.Stateless.BizTalk.Schemas/ContextProperties/Subscribable/BizTalkFactoryProperties.cs
@@ -0,0 +1,50 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2020 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.Diagnostics.CodeAnalysis;
+using Be.Stateless.BizTalk.Schemas.BizTalkFactory;
+
+namespace Be.Stateless.BizTalk.ContextProperties.Subscribable
+{
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Public API.")]
+	public abstract class BizTalkFactoryProperties : Be.Stateless.BizTalk.ContextProperties.BizTalkFactoryProperties
+	{
+		public static readonly MessageContextProperty<CorrelationId, string> CorrelationId
+			= new MessageContextProperty<CorrelationId, string>();
+
+		/// <summary>
+		/// To be used when one application has to be connected to several distinct sets of interacting parties and cannot leak
+		/// messages from one set of parties into another. In concrete terms, to be used in pub/sub of messages in order to keep
+		/// them strictly insulated within an individual set of such interacting parties.
+		/// </summary>
+		public static readonly MessageContextProperty<EnvironmentTag, string> EnvironmentTag
+			= new MessageContextProperty<EnvironmentTag, string>();
+
+		/// <summary>
+		/// Name of the intended receiver of the current message.
+		/// </summary>
+		public static readonly MessageContextProperty<ReceiverName, string> ReceiverName
+			= new MessageContextProperty<ReceiverName, string>();
+
+		/// <summary>
+		/// Name of the initiating sender of the current message.
+		/// </summary>
+		public static readonly MessageContextProperty<SenderName, string> SenderName
+			= new MessageContextProperty<SenderName, string>();
+	}
+}
