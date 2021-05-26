94ffd07c83d4f15a240731723a614ae93e0b0bdf
François Chabot <francois.chabot@stateless.be>
2021-05-26 11:17:02 +0200

    Fix ODX compiler warning X4015
    Symbol Be.Stateless.BizTalk.ContextProperties.Extensions.BizTalkFactoryPropertiesExtensions in assembly Be.Stateless.BizTalk.Schemas is already defined; the first definition is in assembly Be.Stateless.BizTalk.Abstractions
    
    Bump up nugets

--- src/Be.Stateless.BizTalk.Schemas/ContextProperties/Extensions/BizTalkFactorySubscribablePropertiesExtensions.cs
+++ src/Be.Stateless.BizTalk.Schemas/ContextProperties/Extensions/BizTalkFactorySubscribablePropertiesExtensions.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-// Copyright © 2012 - 2020 François Chabot
+// Copyright © 2012 - 2021 François Chabot
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ namespace Be.Stateless.BizTalk.ContextProperties.Extensions
 	/// </summary>
 	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Public API.")]
 	[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "Public API.")]
-	public static class BizTalkFactoryPropertiesExtensions
+	public static class BizTalkFactorySubscribablePropertiesExtensions
 	{
 		public static IBaseMessage SetCorrelationId(this IBaseMessage message, string correlationId)
 		{
